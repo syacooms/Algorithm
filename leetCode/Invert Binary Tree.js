e1947b3321162ea8fa1b28eff5205815c6ee32ef
syacooms <syacooms@gmail.com>
2022-04-12 16:40:16 +0900

    invert binary Tree

---
+++ leetCode/Invert Binary Tree.js
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/invert-binary-tree/
+
+// 트리 부분을 바꾸는 문제 .. dfs 못풀었다.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+  const stack = [root];
+
+  while (stack.length) {
+    const n = stack.pop();
+
+    if (n != null) {
+      [n.left, n.right] = [n.right, n.left];
+      stack.push(n.left, n.right);
+    }
+  }
+
+  return root;
+};
